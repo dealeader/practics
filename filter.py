e26dfad6fe6076fed5ea664da1d133b70897131e
Kirill Miroshnichenko <huglesmafia@gmail.com>
2021-11-17 18:57:36 +0500

    Исправлены замечания по PEP8

--- filter.py
+++ filter.py
@@ -2,39 +2,40 @@ from PIL import Image
 import numpy as np
 import doctest
 
-def get_image_brightness(rows, columns, img_array, mosaic_size) -> int:
+
+def get_image_brightness(rows, columns, image, mosaic_size) -> int:
     """
 
-    :param rows:
-    :param columns:
-    :param img_array:
-    :param mosaic_size:
-    :return:
-ss
+    :param rows: ширина
+    :param columns: высота
+    :param image: обрабатываемое изображение
+    :param mosaic_size: размер мозаики
+    :return: вычисляет и возвращает яркость изображения для дальнейшей обработки
+    
     >>> img_array = np.array(Image.open('donut.jpg'))
-    >>> rows = len(img_array)
-    >>> columns = len(img_array[0])
-    >>> get_image_brightness(rows, columns, img_array, 10)
+    >>> rows = len(image)
+    >>> columns = len(image[0])
+    >>> get_image_brightness(rows, columns, image, 10)
     0
     """
-    return int(np.sum(img_array[rows: rows + mosaic_size, columns: columns + mosaic_size]) // 3 // (mosaic_size ** 2))
+    return int(np.sum(image[rows: rows + mosaic_size, columns: columns + mosaic_size]) // 3 // (mosaic_size ** 2))
 
 
-def create_img_bit(rows, columns, mosaic_size, brightness, img_array) -> list:
+def create_img_bit(rows, columns, mosaic_size, brightness, image) -> list:
     """
 
-    :param rows:
-    :param columns:
-    :param mosaic_size:
-    :param brightness:
-    :param img_array:
-    :return:
+    :param rows: ширина
+    :param columns: высота
+    :param mosaic_size: размер мозаики
+    :param brightness: яркость изображения
+    :param image: обрабатываемое изображение
+    :return: возвращает обработанный кусок изображения
 
-    >>> img_array = np.array(Image.open('donut.jpg'))
-    >>> rows = len(img_array)
-    >>> columns = len(img_array[0])
-    >>> brightness = get_image_brightness(rows, columns, img_array, 10)
-    >>> create_img_bit(rows, columns, 10, brightness, img_array)
+    >>> image = np.array(Image.open('donut.jpg'))
+    >>> rows = len(image)
+    >>> columns = len(image[0])
+    >>> brightness = get_image_brightness(rows, columns, image, 10)
+    >>> create_img_bit(rows, columns, 10, brightness, image)
     array([[[255, 255, 255],
             [255, 255, 255],
             [255, 255, 255],
@@ -85,24 +86,24 @@ def create_img_bit(rows, columns, mosaic_size, brightness, img_array) -> list:
             [255, 255, 255],
             [255, 255, 255]]], dtype=uint8)
     """
-    img_array[rows: rows + mosaic_size, columns: columns + mosaic_size] = np.full(3, brightness)
-    return img_array
+    image[rows: rows + mosaic_size, columns: columns + mosaic_size] = np.full(3, brightness)
+    return image
 
 
-def create_image(img_array, rows_length, columns_length, mosaic_size, grayscale):
+def create_image(image, rows_length, columns_length, mosaic_size, grayscale):
     """
 
-    :param img_array:
-    :param rows_length:
-    :param columns_length:
-    :param mosaic_size:
-    :param grayscale:
-    :return:
+    :param image: обрабатываемое изображение
+    :param rows_length: ширина
+    :param columns_length: высота
+    :param mosaic_size: размер мозаики
+    :param grayscale: градация серого
+    :return: возвращает обработанное изображение в черно-белом формате со сжатием при использовании параметра mosaic_size
 
-    >>> img_array = np.array(Image.open('donut.jpg'))
-    >>> rows = len(img_array)
-    >>> columns = len(img_array[0])
-    >>> create_image(img_array, rows, columns, 10, 50)
+    >>> image = np.array(Image.open('donut.jpg'))
+    >>> rows = len(image)
+    >>> columns = len(image[0])
+    >>> create_image(image, rows, columns, 10, 50)
     array([[[250, 250, 250],
             [250, 250, 250],
             [250, 250, 250],
@@ -156,9 +157,10 @@ def create_image(img_array, rows_length, columns_length, mosaic_size, grayscale)
     for rows in range(0, rows_length, mosaic_size):
         for columns in range(0, columns_length, mosaic_size):
             create_img_bit(rows, columns, mosaic_size,
-                           get_image_brightness(rows, columns, img_array, mosaic_size) // grayscale * grayscale,
-                           img_array)
-    return img_array
+                           get_image_brightness(rows, columns, image, mosaic_size) // grayscale * grayscale,
+                           image)
+    return image
+
 
 doctest.testmod()
 img_array = np.array(Image.open(input('Введите имя изображения в директории: ')))
@@ -167,4 +169,3 @@ Image.fromarray(create_image(img_array,
                              len(img_array[0]),
                              int(input('Введите размер мозаики: ')),
                              int(input('Введите шаг оттенка: ')))).save('res.jpg')
-
